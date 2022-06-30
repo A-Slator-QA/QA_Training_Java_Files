41dd1d979af675fa72348587cf12aed314c6d4a9
A-Slator-QA <andrew.slator95@gmail.com>
2022-06-30 11:08:42 +0100

    Pushing the last Hello World exercise

---
+++ WorldHello/src/WorldHello.java
@@ -0,0 +1,12 @@
+
+public class WorldHello {
+
+	public static void printMessage(String message) {
+		System.out.println(message);
+
+	}
+	public static void main(String[] args) {
+	     printMessage("Big Chungus");
+	 }
+
+}
