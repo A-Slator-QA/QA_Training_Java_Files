6f0d53e3dde5d6edba45da75a11b5a9c81b82a28
A-Slator-QA <andrew.slator95@gmail.com>
2022-06-30 11:08:09 +0100

    Adding the Flowchart exercise

---
+++ Flowchart/src/com/qa/flowchart/Iteration.java
@@ -0,0 +1,63 @@
+package com.qa.flowchart;
+
+public class Iteration {
+
+// Exercise 1:
+//	public static void main(String[] args) {
+//		
+//		for (int A = 100; A < 200; A++) {
+//			System.out.println(A);
+//		}
+//	}
+//
+
+	
+
+// Exercise 2:	
+//	public static void main(String[] args) {
+//		
+//		for (int A = 100; A <= 200; A++) {
+//			if(A % 2 == 0) {
+//				System.out.println("-");
+//				continue;
+//			} 
+//			System.out.println("*");
+//		}
+//	}
+	
+	
+// Exercise 2 Alternate:
+//	public static void main(String[] args) {
+//	int A = 100;{
+//	do {
+//	    if(A%2 == 0){
+//	        System.out.println("-");
+//	    }
+//	    else{
+//	        System.out.println("*");
+//	    }
+//	    A++;
+//	} while(A<= 200);
+//	}}
+
+	
+// Exercise 3:
+//	public static void main(String[] args) {
+//		for (int i = 1; i<=10; i++) {
+//			for (int j = 1; j<=10; j++) {
+//				System.out.println(i);
+//			}
+//		}
+//	}
+	
+
+// Exercise 4:
+//	public static void main(String[] args) {
+//		for (int i = 1; i<=10; i++) {
+//			for (int j = 1; j<=i; j++) {
+//				System.out.println(i);
+//			}
+//		}
+//	}
+	
+}
